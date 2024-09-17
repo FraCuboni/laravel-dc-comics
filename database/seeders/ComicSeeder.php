212c1bbc6135d2cb9b1a5718c8cfe6409907cab4
FraCuboni <fracuboni@gmail.com>
2024-09-17 16:58:44 +0200

    TICKET PUSH

--- database/seeders/ComicSeeder.php
+++ database/seeders/ComicSeeder.php
@@ -13,15 +13,24 @@ class ComicSeeder extends Seeder
      */
     public function run(): void
     {
-        // estraggo dal database (config\comics.php) l'array di dati creando una variabile
+        // estraggo dal database (config\comics.php) l'array associativo di dati creando una variabile
         $comics_db = config('comics');
         //dump visibile con:  php artisan db:seed --class=ComicSeeder
-        dump($comics_db);
+        // dump($comics_db);
 
         // creo ciclo foreach per poter seeddare la table sul server ogni elemento contenuto nell'array
         foreach ($comics_db as $comic) {
+            // crep nuova istanza $new_comic
             $new_comic = new Comic();
-            $new_comic->
+
+            $new_comic->title = $comic['title'];
+            $new_comic->series = $comic['series'];
+            $new_comic->description = $comic['description'];
+            $new_comic->price = $comic['price'];
+            $new_comic->img = $comic['thumb'];
+
+            // salvo i dati appena estratti dall'array nella table sql
+            $new_comic->save();
         }
     }
 }
